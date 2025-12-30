efe91adc0e07329af59d227cb19100a901b56aa6
helios01k <adam.bell.21@portlaoisecbs.com>
2025-12-30 12:40:19 +0100

    idk what i am commiting

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 require("helios")
 print("[main] was successfully initalized")
+
+-- status of the config right now 
+
+-- simple loop rq holup :/
+-- script specs: 
+-- -> the idea is to just while loop 10 times to print out l;5 
+-- it prob is faster but wtv
+
+local x = 0
+
+while x < 10 do
+		x = x + 1
+		print("[CRITICAL - PLEASE READ @ O1K] : The current state of the config is still work in progress. It is still quite buggy, but should be fine.")
+end
